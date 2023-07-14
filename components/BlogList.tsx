7ef1942edbbf8236cc6ee05d333cc1b4fbb3b84c
Farhienza Haikal <farhienzahaikal@gmail.com>
2023-07-15 05:00:35 +0700

    UI Changes

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -34,24 +34,24 @@ export const PageSize = 10;
 const BlogList: React.FC<Props> = () => {
   return (
     <div>
-      <div className="h-auto w-auto mx-auto flex flex-col lg:mx-24 xl:mx-24">
-        <div className=" grid grid-cols-1 mx-auto space-y-5 md:grid-cols-2 md:space-y-5">
+      <div className="h-auto w-auto mx-auto flex flex-col">
+        <div className=" grid grid-cols-1 mx-auto md:grid-cols-2 lg:mx-52">
           {allBlogs.map((post) => (
-            <Card className="flex flex-col xl:mx-10 rounded-xl">
+            <Card className="flex flex-col md:mx-5 mb-5">
               <CardHeader className="">
-                <div className="container h-60 w-90 relative rounded-2xl">
+                <div className="container h-60 w-90 relative ">
                   <Link href={"/blog/" + post.slug} className="container">
                     <Image
                       src={post.thumbnail}
                       alt={post.altthumbnail}
-                      className="rounded-2xl object-cover"
+                      className="rounded-t-lg object-cover"
                       fill
-                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                      />
+                      sizes="(max-width: 384px) 100vw, (max-width: 600px) 50vw, 33vw"
+                    />
                   </Link>
                 </div>
               </CardHeader>
-              <CardTitle className="m-6">
+              <CardTitle className="mx-6 mt-6 mb-12">
                 <Link href={"/blog/" + post.slug}>
                   <h2 className="card-title">{post.title}</h2>
                 </Link>
@@ -59,7 +59,7 @@ const BlogList: React.FC<Props> = () => {
                   {post.tags.map((tag) => (
                     <div className="flex flex-col md:flex-row mx-max"> {/* Use a single div for each tag */}
                       <Link href={"/tags?q=" + tag}>
-                        <Button variant="link" className="justify-start p-1 w-auto h-auto text-xs bg-primary text-accent">
+                        <Button variant="link" className="justify-start p-1 w-auto h-auto text-xs text-foreground">
                           {tag}
                         </Button>
                       </Link>
@@ -67,13 +67,9 @@ const BlogList: React.FC<Props> = () => {
                   ))}
                 </div>
               </CardTitle>
-              <CardContent className="">
-                <div className="h-24 overflow-hidden">{post.description}</div>
-              </CardContent>
             </Card>
           ))}
         </div>
-
       </div>
     </div>
   );
